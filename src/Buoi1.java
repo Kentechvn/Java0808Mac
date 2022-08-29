2eff7a97ca133f17bc09494554d27f9f08c286d5
Kentech <lamquyenanh0124@gmail.com>
2022-08-29 21:14:43 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,10 @@
+public class Buoi1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int Result = a++ - b--;
+        System.out.println("Gia tri bieu thuc la " +Result);
+
+    }
+}
+
